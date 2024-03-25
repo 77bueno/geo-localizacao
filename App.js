9421c1f81a659379cffa52e847c9bbec5630d680
victor <victorhugomotabueno@gmail.com>
2024-03-25 10:43:00 -0300

    Configurações do Location

--- App.js
+++ App.js
@@ -1,25 +1,43 @@
 import { useEffect, useState } from "react";
-import { Alert, Button, StatusBar, StyleSheet, Text, View } from "react-native";
+import {
+  Alert,
+  Button,
+  Image,
+  StatusBar,
+  StyleSheet,
+  Text,
+  View,
+} from "react-native";
 import MapView, { Marker } from "react-native-maps";
 import * as Location from "expo-location";
 
 export default function App() {
+  /* State para monitorar dados da atualização atual do usuário.
+  Inicialmente, nulo. */
   const [minhaLocalizacao, setMinhaLocalizacao] = useState(null);
 
   useEffect(() => {
-    async function obterLocalizacao(){
-      const {status} = await Location.requestForegroundPermissionsAsync();
-      if(status !== "granted"){
+    async function obterLocalizacao() {
+      /* Acessando o status da requisição de permissão de uso
+      dos recursos de geolocalização. */
+      const { status } = await Location.requestForegroundPermissionsAsync();
+
+      /* Se o status NÃO FOR liberado/permitido, então
+      será dado um alerta notificando o usuário. */
+      if (status !== "granted") {
         Alert.alert("Ops!", "Você não autorizou o uso de geolocalização");
         return;
       }
 
+      /* Se o status estiver OK, obtemos os dados da localização
+      atual. E atualizamos o state de minhaLocalizacao. */
       let localizacaoAtual = await Location.getCurrentPositionAsync({});
       setMinhaLocalizacao(localizacaoAtual);
     }
-
     obterLocalizacao();
-  }, [])
+  }, []);
+
+  console.log(minhaLocalizacao);
 
   const [localizacao, setLocalizacao] = useState({
     latitude: -33.867886,
@@ -28,50 +46,47 @@ export default function App() {
     longitudeDelta: 10,
   });
 
+  /* Coordenadas para o MapView */
   const regiaoInicialMapa = {
-    // São Paulo
-    latitude: -23.533773,
-    longitude: -45.65529,
-
-    /* Brasil 
+    /* // Brasil
     latitude: -10,
     longitude: -55, */
+    // São Paulo
+    latitude: -23.533773,
+    longitude: -46.65529,
 
     /* Definição do zoom do mapa.
     Quanto menor, mais próximo o mapa fica.
     Quanto maior, mais longe o mapa fica */
-    latitudeDelta: 0.4,
-    longitudeDelta: 0.4,
+    latitudeDelta: 40,
+    longitudeDelta: 40,
   };
 
   const marcarLocal = (event) => {
-    console.log(event.nativeEvent);
-
     setLocalizacao({
-       ...localizacao, // usado para pegar/manter os deltas
+      ...localizacao, // usado para pegar/manter os deltas
 
-       /* Obtendo novos valores a partir do evento de pressionar */
-      latitude: event.nativeEvent.cordinate.latitude,
-      longitude:  event.nativeEvent.coordinate.longitude,
+      // Obtendo novos valores a partir do evento de pressionar
+      latitude: event.nativeEvent.coordinate.latitude,
+      longitude: event.nativeEvent.coordinate.longitude,
     });
-  }
+  };
 
   return (
     <>
       <StatusBar />
       <View style={estilos.container}>
         <View style={estilos.viewBotao}>
-          <Button title="Onde Estou?" onPress={marcarLocal} />
+          <Button title="Onde estou?" onPress={marcarLocal} />
         </View>
         <View style={estilos.viewMapa}>
-        <MapView
-          style={estilos.mapa}
-          initialRegion={regiaoInicialMapa}
-          mapType="standard"
-          userInterfaceStyle="dark" // somente IOS
-        >
-          <Marker coordinate={localizacao}></Marker>
-        </MapView>
+          <MapView
+            mapType="standard"
+            style={estilos.mapa}
+            initialRegion={regiaoInicialMapa}
+          >
+            <Marker coordinate={localizacao} />
+          </MapView>
         </View>
       </View>
     </>
