49eae5fabffd4af4402d3b5af66461ba6fa95505
victor <victorhugomotabueno@gmail.com>
2024-03-25 09:28:48 -0300

    Marker e ícone personalizado

--- App.js
+++ App.js
@@ -1,10 +1,15 @@
 import { StatusBar, StyleSheet, Text, View } from "react-native";
-import MapView from "react-native-maps";
+import MapView, { Marker } from "react-native-maps";
 
 export default function App() {
   const regiaoInicialMapa = {
+    // São Paulo
+    latitude: -23.533773,
+    longitude: -45.65529,
+
+    /* Brasil 
     latitude: -10,
-    longitude: -55,
+    longitude: -55, */
 
     /* Definição do zoom do mapa.
     Quanto menor, mais próximo o mapa fica.
@@ -12,18 +17,31 @@ export default function App() {
     latitudeDelta: 0.4,
     longitudeDelta: 0.4,
   };
+
+  /* Coordenadas para o Marker que será aplicado ao MapView */
+  const localização = {
+    latitude: -33.867886,
+    longitude: -63.987,
+    latitudeDelta: 10,
+    longitudeDelta: 10,
+  };
   return (
     <>
       <StatusBar />
       <View style={estilos.container}>
         <MapView
-         style={estilos.mapa}
-         initialRegion={regiaoInicialMapa}
-         mapType="hybrid"
-         userInterfaceStyle="dark" // somente IOS
-         maxZoomLevel={15}
-         minZoomLevel={15}
-        />
+          style={estilos.mapa}
+          initialRegion={regiaoInicialMapa}
+          mapType="hybrid"
+          userInterfaceStyle="dark" // somente IOS
+        // maxZoomLevel={15}
+        // minZoomLevel={5}
+        >
+          <Marker coordinate={localizacao}>
+            {/* Ícone personalizado */}
+            <Image source={require("./assets/ghost.png")} />
+          </Marker>
+        </MapView>
       </View>
     </>
   );
